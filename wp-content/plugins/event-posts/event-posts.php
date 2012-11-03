664ff683a1c99c8099a8fc15238b68a49a36be8b
Jonas Frid <frid.jonas@gmail.com>
2012-11-03 20:43:34 +0100

    Förenklat datumhanteringen i event-posts. Istället för att spara en hel drös med metadata sparas istället endast två timestamps. Dessutom: Datuminmatning görs i ett fält istället för två och till detta är jQuery en datetime picker kopplad.

--- wp-content/plugins/event-posts/event-posts.php
+++ wp-content/plugins/event-posts/event-posts.php
@@ -24,7 +24,6 @@ register_activation_hook( __FILE__, 'ep_eventposts_activation' );
 include( 'events-in-page.php' );
 
 function ep_eventposts() {
-
 	/**
 	 * Enable the event custom post type
 	 * http://codex.wordpress.org/Function_Reference/register_post_type
@@ -68,69 +67,56 @@ add_action( 'init', 'ep_eventposts' );
  * Two avoid repeating code, we'll just pass the $identifier in a callback.
  * If you wanted to add this to regular posts instead, just swap 'event' for 'post' in add_meta_box.
  */
-
 function ep_eventposts_metaboxes() {
-	add_meta_box( 'ept_event_date_start', 'Start Date and Time', 'ept_event_date', 'event', 'side', 'default', array( 'id' => '_start') );
-	add_meta_box( 'ept_event_date_end', 'End Date and Time', 'ept_event_date', 'event', 'side', 'default', array('id'=>'_end') );
+	// Enqueue jQuery datepicker used for the event dates
+	wp_enqueue_script( 'jquery-ui-datepicker' );
+	wp_enqueue_style( 'jquery.ui.theme', plugins_url( '/jquery-ui-theme-flick/jquery-ui-1.9.1.custom.css', __FILE__ ) );
+
+	add_meta_box( 'ept_metabox_event_date_start', 'Start Date and Time', 'ept_metabox_event_date', 'event', 'side', 'default', array( 'id' => '_start') );
+	add_meta_box( 'ept_metabox_event_date_end', 'End Date and Time', 'ept_metabox_event_date', 'event', 'side', 'default', array('id'=>'_end') );
 	add_meta_box( 'ept_event_location', 'Event Location', 'ept_event_location', 'event', 'normal', 'default', array('id'=>'_end') );
 }
 add_action( 'admin_init', 'ep_eventposts_metaboxes' );
 
-// Metabox HTML
 
-function ept_event_date($post, $args) {
+// Metabox HTML
+function ept_metabox_event_date($post, $args) {
 	$metabox_id = $args['args']['id'];
 	global $post, $wp_locale;
 
 	// Use nonce for verification
 	wp_nonce_field( plugin_basename( __FILE__ ), 'ep_eventposts_nonce' );
 
-	$time_adj = current_time( 'timestamp' );
-	$month = get_post_meta( $post->ID, $metabox_id . '_month', true );
-
-	if ( empty( $month ) ) {
-		$month = gmdate( 'm', $time_adj );
-	}
-
-	$day = get_post_meta( $post->ID, $metabox_id . '_day', true );
-
-	if ( empty( $day ) ) {
-		$day = gmdate( 'd', $time_adj );
-	}
-
-	$year = get_post_meta( $post->ID, $metabox_id . '_year', true );
-
-	if ( empty( $year ) ) {
-		$year = gmdate( 'Y', $time_adj );
+	// $time_adj = current_time( 'timestamp' );
+	$timestamp = get_post_meta( $post->ID, $metabox_id . '_timestamp', true );
+	if ( $timestamp == '' ) {
+		$timestamp = time();
 	}
 	
-	$hour = get_post_meta($post->ID, $metabox_id . '_hour', true);
- 
-    if ( empty($hour) ) {
-        $hour = gmdate( 'H', $time_adj );
-    }
- 
-    $min = get_post_meta($post->ID, $metabox_id . '_minute', true);
- 
-    if ( empty($min) ) {
-        $min = '00';
-    }
+	$year = date( 'Y', $timestamp );
+	$month = date( 'n', $timestamp );
+	$day = date( 'd', $timestamp );
+	$hour = date( 'H', $timestamp );
+	$min = date( 'i', $timestamp );
 
-	$month_s = '<select name="' . $metabox_id . '_month">';
-	for ( $i = 1; $i < 13; $i = $i +1 ) {
-		$month_s .= "\t\t\t" . '<option value="' . zeroise( $i, 2 ) . '"';
-		if ( $i == $month )
-			$month_s .= ' selected="selected"';
-		$month_s .= '>' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) . "</option>\n";
-	}
-	$month_s .= '</select>';
+	$date = $year . '-' . $month . '-' . $day;
+	echo '<input type="text" class="datepicker" name="' . $metabox_id . '_date" value="' . $date . '" size="10" maxlength="10" />';
 
-	echo $month_s;
-	echo '<input type="text" name="' . $metabox_id . '_day" value="' . $day  . '" size="2" maxlength="2" />';
-    echo '<input type="text" name="' . $metabox_id . '_year" value="' . $year . '" size="4" maxlength="4" /> @ ';
-    echo '<input type="text" name="' . $metabox_id . '_hour" value="' . $hour . '" size="2" maxlength="2"/>:';
-    echo '<input type="text" name="' . $metabox_id . '_minute" value="' . $min . '" size="2" maxlength="2" />';
- 
+	// echo '<input type="text" name="' . $metabox_id . '_year" value="' . $year . '" size="4" maxlength="4" />';
+
+	// $month_s = '<select name="' . $metabox_id . '_month">';
+	// for ( $i = 1; $i <= 12; $i++ ) {
+	// 	$month_s .= "\t\t\t" . '<option value="' . zeroise( $i, 2 ) . '"';
+	// 	if ( $i == $month )
+	// 		$month_s .= ' selected="selected"';
+	// 	$month_s .= '>' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) . "</option>\n";
+	// }
+	// $month_s .= '</select>';
+
+	// echo $month_s;
+	// echo '<input type="text" name="' . $metabox_id . '_day" value="' . $day  . '" size="2" maxlength="2" />';
+	echo '<input type="text" name="' . $metabox_id . '_hour" value="' . $hour . '" size="2" maxlength="2"/>:';
+	echo '<input type="text" name="' . $metabox_id . '_minute" value="' . $min . '" size="2" maxlength="2" />';
 }
 
 function ept_event_location() {
@@ -150,51 +136,41 @@ function ep_eventposts_save_meta( $post_id, $post ) {
 	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
 		return;
 
-	if ( !isset( $_POST['ep_eventposts_nonce'] ) )
+	if ( ! isset( $_POST['ep_eventposts_nonce'] ) )
 		return;
 
-	if ( !wp_verify_nonce( $_POST['ep_eventposts_nonce'], plugin_basename( __FILE__ ) ) )
+	if ( ! wp_verify_nonce( $_POST['ep_eventposts_nonce'], plugin_basename( __FILE__ ) ) )
 		return;
 
 	// Is the user allowed to edit the post or page?
-	if ( !current_user_can( 'edit_post', $post->ID ) )
+	if ( ! current_user_can( 'edit_post', $post->ID ) )
 		return;
 
 	// OK, we're authenticated: we need to find and save the data
 	// We'll put it into an array to make it easier to loop though
-	
 	$metabox_ids = array( '_start', '_end' );
 
 	foreach ( $metabox_ids as $key ) {
-	    
-	    $aa = $_POST[$key . '_year'];
-		$mm = $_POST[$key . '_month'];
-		$jj = $_POST[$key . '_day'];
-		$hh = $_POST[$key . '_hour'];
-		$mn = $_POST[$key . '_minute'];
+		$year   = $_POST[$key . '_year'];
+		$month  = $_POST[$key . '_month'];
+		$day    = $_POST[$key . '_day'];
+		$hour   = $_POST[$key . '_hour'];
+		$min    = $_POST[$key . '_minute'];
 		
-		$aa = ( $aa <= 0 ) ? date('Y') : $aa;
-		$mm = ( $mm <= 0 ) ? date('n') : $mm;
-		$jj = sprintf( '%02d', $jj );
-		$jj = ( $jj > 31 ) ? 31 : $jj;
-		$jj = ( $jj <= 0 ) ? date('j') : $jj;
-		$hh = sprintf( '%02d', $hh );
-		$hh = ( $hh > 23 ) ? 23 : $hh;
-		$mn = sprintf( '%02d', $mn );
-		$mn = ( $mn > 59 ) ? 59 : $mn;
+		$year = ( $year < 0 ) ? date('Y') : $year;
+		$month = ( $month <= 0 || $month > 12 ) ? date('n') : $month;
+		$day = sprintf( '%02d', $day );
+		$day = ( $day > 31 ) ? 31 : $day;
+		$day = ( $day <= 0 ) ? date('j') : $day;
+		$hour = sprintf( '%02d', $hour );
+		$hour = ( $hour > 23 ) ? 23 : $hour;
+		$min = sprintf( '%02d', $min );
+		$min = ( $min > 59 ) ? 59 : $min;
 		
-		$events_meta[$key . '_year'] = $aa;
-		$events_meta[$key . '_month'] = $mm;
-		$events_meta[$key . '_day'] = $jj;
-		$events_meta[$key . '_hour'] = $hh;
-		$events_meta[$key . '_minute'] = $mn;
-	    $events_meta[$key . '_eventtimestamp'] = $aa . $mm . $jj . $hh . $mn;
-	    
+		$events_meta[ $key . '_timestamp' ] = mktime( $hour, $min, 0, $month, $day, $year );
     }
  
-
 	// Add values of $events_meta as custom fields
-
 	foreach ( $events_meta as $key => $value ) { // Cycle through the $events_meta array!
 		// Don't store custom data twice
 		if ( $post->post_type == 'revision' )
@@ -214,10 +190,9 @@ function ep_eventposts_save_meta( $post_id, $post ) {
 		if ( !$value )
 			delete_post_meta( $post->ID, $key );
 	}
-
 }
 
-add_action( 'save_post', 'ep_eventposts_save_meta', 1, 2 );
+add_action( 'save_post', 'ep_eventposts_save_meta', $priority = 1, $accepted_args = 2 );
 
 /**
  * Helpers to display the date on the front end
@@ -272,17 +247,17 @@ function ep_event_query( $query ) {
 
 	global $wp_the_query;
 	
-	if ( $wp_the_query === $query && !is_admin() && is_post_type_archive( 'event' ) ) {
+	if ( $wp_the_query === $query && ! is_admin() && is_post_type_archive( 'event' ) ) {
 		$meta_query = array(
 			array(
-				'key' => '_start_eventtimestamp',
+				'key' => '_start_timestamp',
 				'value' => $current_timestamp,
 				'compare' => '>'
 			)
 		);
 		$query->set( 'meta_query', $meta_query );
 		$query->set( 'orderby', 'meta_value_num' );
-		$query->set( 'meta_key', '_start_eventtimestamp' );
+		$query->set( 'meta_key', '_start_timestamp' );
 		$query->set( 'order', 'ASC' );
 		$query->set( 'posts_per_page', '2' );
 	}
@@ -291,4 +266,21 @@ function ep_event_query( $query ) {
 
 add_action( 'pre_get_posts', 'ep_event_query' );
 
+function ept_admin_footer_datepicker() {
+	?>
+	<script type="text/javascript">
+	jQuery(document).ready(function(){
+		jQuery('.datepicker').datepicker({
+			dateFormat : 'yy-mm-dd',
+			firstDay: 1,
+			showWeek: true,
+			showOn: "button",
+			buttonImage: "<?php echo plugin_dir_url( __FILE__ ) . 'jquery-ui-theme-flick/images/calendar.gif'; ?>",
+			buttonImageOnly: false
+		});
+	});
+	</script>
+	<?php
+}
+add_action( 'admin_footer', 'ept_admin_footer_datepicker' );
 ?>
